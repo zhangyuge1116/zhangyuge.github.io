65d0206afd12d08232d3f790f152fe3238985e16
zhangyuge1116 <zhangyuge1116@foxmail.com>
2024-09-12 20:12:25 +0800

    first use github
    first use github

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/main.c.obj"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.lib"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
